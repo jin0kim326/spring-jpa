bc86d61b04e26d53dcde43726e32477643ce1c73
김진영C <jin0kim326@douzone.com>
2022-10-13 08:52:28 +0900

    주문 시스템 엔티티개발#1

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // Order에 있는 member 필드에 의해 매핑되었음. -> Order의 member가 연관관계의 주인
+    private List<Order> orders = new ArrayList<>();
+}
