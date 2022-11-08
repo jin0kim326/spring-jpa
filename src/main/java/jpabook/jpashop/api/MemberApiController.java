76eb27ec14a05bbc52af5e3e432b6ab5aaa4caff
김진영C <jin0kim326@douzone.com>
2022-11-08 12:53:57 +0900

    API#2 - 회원수정

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -5,9 +5,7 @@ import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -40,14 +38,35 @@ public class MemberApiController {
          */
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse editMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
      @Data
      static class CreateMemberRequest {
         private String name;
      }
 
     @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
     @AllArgsConstructor
     static class CreateMemberResponse {
         private Long id;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 }
